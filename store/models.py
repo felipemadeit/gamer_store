b2e99f8e6d698b970e98ca43c7180a3f9b566864
felipemadeit <felipemadeit@gmailcom>
2024-06-10 19:11:30 -0500

    New model product and changes in the database

--- store/models.py
+++ store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product (models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, default='non-category')
+    description = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
